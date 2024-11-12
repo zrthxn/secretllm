549e75da9c4d1ad95ffec6067f0587f5eacedd7b
Alisamar Husain <zrthxn@gmail.com>
2024-11-13 00:12:12 +0100

    Fix playground

--- 04-transformers/playground.py
+++ 04-transformers/playground.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 from pathlib import Path
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import pipeline
 
 
@@ -13,34 +13,26 @@ DIRS = Path(".").absolute() / ".checkpoints"
 # The hardware device you want to use. Use "cuda" if you have an NVIDIA GPU. Use "mps" if you're on an M series Mac.
 DEVICE = "mps"
 
+"""
+# Playground
+"""
+
 # Automatic discovery of models
 available_models = {}
 for name in os.listdir(DIRS):
     if (DIRS / name).is_dir() and "model.safetensors" in os.listdir(DIRS / name):
         available_models[name] = DIRS / name
 
-model_name = list(available_models.keys())[0]
-
-model = AutoModelForCausalLM.from_pretrained(available_models[model_name])
-tokenizer = AutoTokenizer.from_pretrained(available_models[model_name] / "tokenizer")
-
-def run_completion(prompt: str, max_new_tokens: int = 100) -> str:
-    generate = pipeline("text-generation", model=model, tokenizer=tokenizer, device=DEVICE)
-    output = generate(prompt, max_new_tokens = max_new_tokens)[0]
-    return output["generated_text"]
-
-"""
-# Playground
-"""
-
 prompt_col, model_col = st.columns((9, 3))
-with prompt_col:
-    prompt = st.text_input("Prompt", placeholder="Write a prompt in the model's language", key="prompt")
-    output = run_completion(prompt)
 with model_col:
     model_name = st.selectbox("Model", options=available_models.keys(), index=0)
-    
+
     model = AutoModelForCausalLM.from_pretrained(available_models[model_name])
     tokenizer = AutoTokenizer.from_pretrained(available_models[model_name] / "tokenizer")
+
+with prompt_col:
+    prompt = st.text_input("Prompt", placeholder="Write a prompt in the model's language", key="prompt")
+    generate = pipeline("text-generation", model=model, tokenizer=tokenizer, device=DEVICE)
+    output = generate(prompt, max_new_tokens = 250)[0]["generated_text"]
 
 st.text_area("Generated", placeholder="Generated text comes here", height=300, value=output)
